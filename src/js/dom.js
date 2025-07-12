4c4abc821c8520d3209d9722e7dace6087b57f18
Aleskoss <187804686+Aleskoss@users.noreply.github.com>
2025-07-12 19:24:31 +0200

    Add button for adding todos

--- src/js/dom.js
+++ src/js/dom.js
@@ -3,7 +3,7 @@ export const dom = (() => {
   const projectsDiv = document.querySelector('#projects')
   const todosDiv = document.querySelector('#todos')
   const dialog = document.createElement('dialog')
-  const loadProjects = () => {
+  const loadProjects = (project) => {
     const projectsLength = projects.getProjects().length
     for(let i = 0; i < projectsLength; i++){
       const para = document.createElement('p')
@@ -11,6 +11,7 @@ export const dom = (() => {
       projectsDiv.appendChild(para)
     }
     addProjectBtn()
+    addTodoBtn()
   }
   const loadTodos = (project) => {
   }
@@ -23,9 +24,10 @@ export const dom = (() => {
   const addProjectDialogDisplay = () => {
     document.body.appendChild(dialog)
     const addProjectBtn = document.createElement('button')
+    addProjectBtn.textContent = 'Add'
     const projectTitle = document.createElement('input')
-    dialog.appendChild(addProjectBtn)
     dialog.appendChild(projectTitle)
+    dialog.appendChild(addProjectBtn)
     dialog.showModal()
     addProjectBtn.addEventListener('click', () => {
       projects.addProjectToProjects(projectTitle.value)
@@ -38,8 +40,27 @@ export const dom = (() => {
       container.removeChild(container.lastChild)
     }
   }
-  const addTodoBtn = () => {
+  const addTodoBtn = (project) => {
     const btn = document.createElement('button')
+    btn.textContent = '+'
+    todosDiv.appendChild(btn)
+    btn.addEventListener('click',addTodoDialogDisplay)
+  }
+  const addTodoDialogDisplay = () => {
+    const addTodoBtn = document.createElement('button')
+    addTodoBtn.textContent = 'Add'
+    const title = document.createElement('input')
+    const description = document.createElement('input')
+    dialog.appendChild(title)
+    dialog.appendChild(description)
+    dialog.appendChild(addTodoBtn)
+    document.body.appendChild(dialog)
+    dialog.showModal()
+    addTodoBtn.addEventListener('click',() => {
+      projects.addToDoToProject(projects.currentProject("Default"),title,description,"dueDate","priority")
+      deleteContainerContent(dialog)
+      dialog.close()
+    })
   }
     return {loadProjects,loadTodos,addProjectBtn}
 })()
